4e0a7ba8fadd50875cccda134a1df474a0cf37f4
khealey <khealey@msu.edu>
2016-08-24 13:14:34 -0400

    moved recall scoring code to psiturk toolbox

--- static/analyses/run_analyses.py
+++ static/analyses/run_analyses.py
@@ -3,13 +3,11 @@ from beh_tools import recall_dynamics as rdf
 from anal_funcs import *
 import pandas as pd
 
-# load the data from the psiturk experiment database
+# load the data from the psiturk experiment database and make it into a free recall object
 db_url = "mysql://khealey:Bib96?reply@127.0.0.1:3306/fr_turk_debug"
 table_name = 'FRresults'
 data = psiturk_tools.load_psiturk_data(db_url, table_name)
-
-# postprocess the data --- make it into a free recall object
-recalls = make_recall_matrix(data)
+recalls = psiturk_tools.make_psiturk_recall_matrix(data)
 
 # compute a SPC
 spc = rdf.spc()
