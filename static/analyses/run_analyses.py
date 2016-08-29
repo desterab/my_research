7d7b641a2822c5eeac5d219a31bba88198a3af9f
khealey <khealey@msu.edu>
2016-08-29 13:31:53 -0400

    seems to be working!

--- static/analyses/run_analyses.py
+++ static/analyses/run_analyses.py
@@ -1,16 +1,18 @@
+import sys,os,os.path
+sys.path.append("/home/khealey/code/py_modules/")
 from beh_tools import psiturk_tools
-from beh_tools import recall_dynamics as rdf
 from anal_funcs import *
 import pandas as pd
 
 # load the data from the psiturk experiment database and make it into a free recall object
-db_url = "mysql://khealey:Bib96?reply@35.8.48.32:3306/turkFR"
+db_url = "mysql://khealey:Bib96?reply@127.0.0.1/for_debuging"
 table_name = 'FRresults'
 data = psiturk_tools.load_psiturk_data(db_url, table_name)
 recalls = psiturk_tools.make_psiturk_recall_matrix(data)
 
 # compute a SPC
-spc = rdf.spc()
+# from beh_tools import recall_dynamics as rdf
+# spc = rdf.spc()
 
 
 
