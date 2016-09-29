b7d0d9a4ed49cd8ed2b990331fce3b3f0f9f3825
khealey <khealey@msu.edu>
2016-09-29 11:33:16 -0400

    adding code for figures

--- static/analyses/run_analyses.py
+++ static/analyses/run_analyses.py
@@ -25,18 +25,14 @@ lists = recalls.list.unique()
 all_crps = pd.DataFrame()
 for s in subjects:
     for l in lists:
+
+        # get the data for just this list
         s_filter = recalls.subject == s
         l_filter = recalls.list == l
         cur_recalls = recalls.loc[s_filter & l_filter, :]
-        if cur_recalls.shape[0] == 0:
-            continue
-        rec_mat = cur_recalls.as_matrix(range(recalls.shape[1] - 2))
-        crp = rdf.crp(listlen=16, recalls=rec_mat, filter_ind=None, allow_repeats=False, exclude_op=0)
-        crp = pd.DataFrame.from_records(crp)
 
-        # skip lists with zero recall accuracy
-        prec = rdf.prec(listlen=16, recalls=rec_mat)
-        if prec <= 0:
+        # skip if there were no recalls
+        if cur_recalls.shape[0] == 0:
             continue
 
         # skip lists if subject reported being aware
@@ -45,6 +41,22 @@ for s in subjects:
         if aware.any() and implicit:
             continue
 
+        # skip lists with zero recall accuracy
+        rec_mat = cur_recalls.as_matrix(range(recalls.shape[1] - 2))  # format recalls matrix for use with rdf functions
+        prec = rdf.prec(listlen=16, recalls=rec_mat)
+        if prec <= 0:
+            continue
+
+        # compute temporal factor, getting average seperatley for positive and negative lags
+        tempf = rdf.tem_fact(listlen=16, recalls=rec_mat, filter_ind=None)
+        tempf = pd.DataFrame.from_records(tempf)
+        pos_tf = tempf.tf[tempf.lag > 0].mean()
+        neg_tf = tempf.tf[tempf.lag < 0].mean()
+
+        # compute crp
+        crp = rdf.crp(listlen=16, recalls=rec_mat, filter_ind=None, allow_repeats=False, exclude_op=0)
+        crp = pd.DataFrame.from_records(crp)
+
         # add list number, and condition ids
         crp['subject'] = pd.Series([s for x in range(len(crp.index))], index=crp.index)
         crp['list'] = pd.Series([l for x in range(len(crp.index))], index=crp.index)
@@ -52,7 +64,12 @@ for s in subjects:
                                                  index=crp.index)
         crp['task_condition'] = pd.Series([cur_recalls.task_condition[0] for x in range(len(crp.index))],
                                           index=crp.index)
-        crp['prec'] = pd.Series([prec for x in range(len(crp.index))], index=crp.index) # todo: this needlessly makes a copy of prec for each lag... this seems like a xarray issue
+        crp['prec'] = pd.Series([prec for x in range(len(crp.index))],
+                                index=crp.index)  # todo: this needlessly makes a copy of prec for each lag... this seems like a xarray issue
+        crp['pos_tf'] = pd.Series([pos_tf for x in range(len(crp.index))],
+                                index=crp.index)
+        crp['neg_tf'] = pd.Series([neg_tf for x in range(len(crp.index))],
+                                index=crp.index)
 
         all_crps = pd.concat([all_crps, crp])
 
@@ -79,7 +96,6 @@ print pd.crosstab(all_crps.lag, [all_crps.instruction_condition, all_crps.task_c
 import matplotlib.pyplot as plt
 from matplotlib import gridspec
 
-
 # overall figure style
 # sns.set_style("white")
 sns.set_style("whitegrid")
@@ -103,14 +119,14 @@ pfr_axis = fig1.add_subplot(gs[0, 2])
 crp_axis = fig1.add_subplot(gs[1:4, :])
 tempf_axis = fig1.add_subplot(gs[4:, :])
 
-
 # bar plot of prec by task
-data_filter = np.logical_and(all_crps.instruction_condition == 1, np.logical_and(all_crps.lag.abs() == 5, all_crps.list == 0))
+data_filter = np.logical_and(all_crps.instruction_condition == 1,
+                             np.logical_and(all_crps.lag.abs() == 5, all_crps.list == 0))
 sns.barplot(x="task_condition", y="prec", data=all_crps.loc[data_filter, :], ax=prec_axis)
 sns.despine()
 
 # plot crps
-data_filter = np.logical_and(all_crps.instruction_condition == 1, np.logical_and(all_crps.lag.abs() <= 5, all_crps.list == 0))
+data_filter = np.logical_and(all_crps.instruction_condition == 1,
+                             np.logical_and(all_crps.lag.abs() <= 5, all_crps.list == 0))
 sns.factorplot(x="lag", y="crp", hue="task_condition", data=all_crps.loc[data_filter, :], ax=crp_axis)
 sns.despine()
-
