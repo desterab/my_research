4a2f0c8cf4f366bd0576bccad82f26b9d32a8fa8
khealey <khealey@msu.edu>
2016-10-06 12:00:01 -0400

    about to try paralleling perm code

--- static/analyses/correction_analyses.py
+++ static/analyses/correction_analyses.py
@@ -26,17 +26,18 @@ params = {"n_lists": 100,
 
 #  run a bunch of subjects
 n_ss = 10
+n_perms = 100
 
 # at a bunch of levels of prec
 these_precs = np.array([.75, .5, .25])
 
-conditions = [("lo_p", "med_c"), ("med_p", "med_c"), ("hi_p", "med_c")]
+conditions = [("lo_p", "hi_c"), ("med_p", "med_c"), ("hi_p", "lo_c")]
 
 cond_params = {"lo_p": .25,
                "med_p": .5,
                "hi_p": .75,
                "lo_c": .5,
-               "med_c": 1,
+               "med_c": 1.0,
                "hi_c": 1.5}
 
 asym_factor = 1
@@ -44,47 +45,95 @@ asym_factor = 1
 
 prec_results = pd.DataFrame()
 tempf_results = pd.DataFrame()
+tempf_z_results = pd.DataFrame()
 spc_results = pd.DataFrame()
 crp_results = pd.DataFrame()
-for p_i, cur_prec in enumerate(these_precs):
-    print p_i
+crp_z_results = pd.DataFrame()
+true_results = pd.DataFrame()
+for cond_i in conditions:
+    print cond_i
+    params['p_rec'] = cond_params[cond_i[0]]
+    params['a_pos'] = cond_params[cond_i[1]]
+    params["a_neg"] = params['a_pos'] * asym_factor
+
+
     for s_i in range(n_ss):
-        params['p_rec'] = cur_prec
+
+        # run the model
         recalls = random_transition(params)
 
         # compute actual prec
         prec = rdf.prec(listlen=16, recalls=recalls)
-        prec = pd.DataFrame([[s_i, cur_prec, prec]], columns=['subject', 'param_prec', 'observed_prec'])
+        prec = pd.DataFrame([[s_i, params['p_rec'], prec]], columns=['subject', 'param_prec', 'observed_prec'])
         prec_results = pd.concat([prec_results, prec])
 
         # compute spc
         spc = rdf.spc(listlen=16, recalls=recalls)
         spc = pd.DataFrame.from_records(spc)
         spc['subject'] = pd.Series([s_i for x in range(len(spc.index))], index=spc.index)
-        spc['param_prec'] = pd.Series([p_i for x in range(len(spc.index))], index=spc.index)
+        spc['param_prec'] = pd.Series([params['p_rec'] for x in range(len(spc.index))], index=spc.index)
         spc_results = pd.concat([spc_results, spc])
 
         # compute crp
         crp = rdf.crp(listlen=16, recalls=recalls)
         crp = pd.DataFrame.from_records(crp)
         crp['subject'] = pd.Series([s_i for x in range(len(crp.index))], index=crp.index)
-        crp['param_prec'] = pd.Series([p_i for x in range(len(crp.index))], index=crp.index)
+        crp['param_prec'] = pd.Series([params['p_rec'] for x in range(len(crp.index))], index=crp.index)
         crp_results = pd.concat([crp_results, crp])
+
+        # compute random ctrl crp
+        crp_z = rdf.relative_to_random(listlen=16, recalls=recalls, filter_ind=None, statistic_func=rdf.crp,
+                                       data_col="crp", n_perms=n_perms)
+        crp_z = pd.DataFrame.from_records(crp_z)
+        crp_z['subject'] = pd.Series([s_i for x in range(len(crp.index))], index=crp_z.index)
+        crp_z['param_prec'] = pd.Series([params['p_rec'] for x in range(len(crp_z.index))], index=crp_z.index)
+        crp_z_results = pd.concat([crp_z_results, crp_z])
 
         # compute temporal factor
         tempf = rdf.tem_fact(listlen=16, recalls=recalls)
         tempf = np.nanmean(tempf['tf'])
-        tempf = pd.DataFrame([[s_i, cur_prec, tempf]], columns=['subject', 'param_prec', 'observed_tempf'])
+        tempf = pd.DataFrame([[s_i, params['p_rec'], tempf]], columns=['subject', 'param_prec', 'observed_tempf'])
         tempf_results = pd.concat([tempf_results, tempf])
+
+        # compute random temporal factor
+        tempf_z = rdf.relative_to_random(listlen=16, recalls=recalls, filter_ind=None, statistic_func=rdf.tem_fact,
+                                         data_col="tf", n_perms=n_perms)
+        tempf_z = pd.DataFrame.from_records(tempf_z)
+        tempf_z = tempf_z.tf.mean()
+        tempf_z = pd.DataFrame([[s_i, params['p_rec'], tempf_z]], columns=['subject', 'param_prec', 'observed_tempf_z'])
+        tempf_z_results  = pd.concat([tempf_z_results, tempf_z])
+
+        # true contiguity implied by model param
+        pos_lag_probs = params["asym"] * (np.arange(params["n_items"] - 1) + 1) ** -params["a_pos"]
+        neg_lag_probs = (np.arange(params["n_items"] - 1) + 1) ** -params["a_neg"]
+        lag_probs = np.concatenate((neg_lag_probs[::-1], pos_lag_probs))
+        lag_probs = pd.DataFrame(lag_probs, columns=['b'])
+        lags = np.concatenate((np.arange(-params["n_items"] + 1, 0), np.arange(1, params["n_items"])))
+        lag_probs['param_prec'] = pd.Series([params['p_rec'] for x in range(len(lag_probs.index))], index=lag_probs.index)
+        lag_probs['lag'] = pd.Series([x for x in lags], index=lag_probs.index)
+        true_results = pd.concat([true_results, pd.DataFrame([[np.nan, 0, params['p_rec']]], columns=['b', 'lag', 'param_prec']),
+                                  lag_probs])
 
 sns.factorplot(x='param_prec', y='observed_tempf', data=tempf_results)
 # plt.savefig('tempf' + '.pdf')
 
+data_fiter = true_results.lag.abs() <= 5
+sns.factorplot(x="lag", y='b', hue="param_prec", data=true_results.loc[data_fiter, :])
+
 data_fiter = crp_results.lag.abs() <= 5
 sns.factorplot(x="lag", y='crp', hue="param_prec", data=crp_results.loc[data_fiter, :])
 
+data_fiter = crp_z_results.lag.abs() <= 5
+sns.factorplot(x="lag", y='crp', hue="param_prec", data=crp_z_results.loc[data_fiter, :])
+
+
+
+
+
+
 sns.factorplot(x="serial_pos", y='prec', hue="param_prec", data=spc_results)
 
+x=1
 
 
 
@@ -96,43 +145,44 @@ sns.factorplot(x="serial_pos", y='prec', hue="param_prec", data=spc_results)
 
 
 
-##### tcm version
-#  run a bunch of subjects
-n_ss = 1000
-
-# at a bunch of levels of prec
-these_precs = np.linspace(start=0., stop=1., num=20)
-
-# model parameters
-params = {"n_lists": 100,
-          "n_items": 16,
-          "n_recall_attempts": 20,
-          "b_enc": 0.561,
-          "b_rec": 0.375,
-          "p_rec": 0.5}
-
-
-prec_results = pd.DataFrame()
-tempf_results = pd.DataFrame()
-for p_i, cur_prec in enumerate(these_precs):
-    print p_i
-    for s_i in range(n_ss):
-        params['p_rec'] = cur_prec
-        recalls = run_model(params)
-
-        # compute actual prec
-        prec = rdf.prec(listlen=16, recalls=recalls)
-        prec = pd.DataFrame([[s_i, cur_prec, prec]], columns=['subject', 'param_prec', 'observed_prec'])
-        prec_results = pd.concat([prec_results, prec])
-
-        # compute temporal factor
-        tempf = rdf.tem_fact(listlen=16, recalls=recalls)
-        tempf = np.nanmean(tempf['tf'])
-        tempf = pd.DataFrame([[s_i, cur_prec, tempf]], columns=['subject', 'param_prec', 'observed_tempf'])
-        tempf_results = pd.concat([tempf_results, tempf])
-
-
-
-
-sns.factorplot(x='param_prec', y='observed_tempf', data=tempf_results)
-plt.savefig('tempf' + '.pdf')
+#
+# ##### tcm version
+# #  run a bunch of subjects
+# n_ss = 1000
+#
+# # at a bunch of levels of prec
+# these_precs = np.linspace(start=0., stop=1., num=20)
+#
+# # model parameters
+# params = {"n_lists": 100,
+#           "n_items": 16,
+#           "n_recall_attempts": 20,
+#           "b_enc": 0.561,
+#           "b_rec": 0.375,
+#           "p_rec": 0.5}
+#
+#
+# prec_results = pd.DataFrame()
+# tempf_results = pd.DataFrame()
+# for p_i, cur_prec in enumerate(these_precs):
+#     print p_i
+#     for s_i in range(n_ss):
+#         params['p_rec'] = cur_prec
+#         recalls = run_model(params)
+#
+#         # compute actual prec
+#         prec = rdf.prec(listlen=16, recalls=recalls)
+#         prec = pd.DataFrame([[s_i, cur_prec, prec]], columns=['subject', 'param_prec', 'observed_prec'])
+#         prec_results = pd.concat([prec_results, prec])
+#
+#         # compute temporal factor
+#         tempf = rdf.tem_fact(listlen=16, recalls=recalls)
+#         tempf = np.nanmean(tempf['tf'])
+#         tempf = pd.DataFrame([[s_i, cur_prec, tempf]], columns=['subject', 'param_prec', 'observed_tempf'])
+#         tempf_results = pd.concat([tempf_results, tempf])
+#
+#
+#
+#
+# sns.factorplot(x='param_prec', y='observed_tempf', data=tempf_results)
+# plt.savefig('tempf' + '.pdf')
--- static/analyses/run_analyses.py
+++ static/analyses/run_analyses.py
@@ -3,22 +3,25 @@ import pickle
 from anal_funcs import *
 
 
-remake_data_file = False
+remake_data_file = True
 
 # load or create the data
 if os.path.isfile("all_crps.pkl") and not remake_data_file:
     all_crps = pickle.load(open("all_crps.pkl", "rb"))
     all_spcs = pickle.load(open("all_spcs.pkl", "rb"))
 else:
-    all_crps, all_spcs = load_the_data(n_perms=1000)
+    all_crps, all_spcs = load_the_data(n_perms=1)
     all_crps.to_pickle("all_crps.pkl")
     all_spcs.to_pickle("all_spcs.pkl")
 
-# check sample sizes
-print pd.crosstab(all_crps.lag, [all_crps.instruction_condition, all_crps.task_condition])
+# take mean within subject and lag, check sample sizes
+all_crps = all_crps.groupby(['subject', 'lag', 'list'], as_index=False).mean()
+all_spcs = all_spcs.groupby(['subject', 'serial_pos', 'list'], as_index=False).mean()
+
+print pd.crosstab(all_crps[np.logical_and(all_crps.lag==0,all_crps.list==0)].lag, [all_crps.instruction_condition, all_crps.task_condition])
 
 # make the figures
-apply_perm_correction = True
+apply_perm_correction = False
 if apply_perm_correction:
     fig_prefix = "_perm"
 else:
