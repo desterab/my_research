5c1030b09f8ba6aa2e59bd66db53b18f0177ec18
khealey <khealey@msu.edu>
2018-03-11 13:20:35 -0400

    end of day fri

--- analyses/run_analyses.py
+++ analyses/run_analyses.py
@@ -12,14 +12,14 @@ results_dir = "../dissemination/manuscript/jml/second_submission/figures/"
 dict_path = "/Users/khealey/code/py_modules/cbcc_tools/wordpool_files/websters_dict.txt"
 
 # number of permutations for permutations tests
-n_perms = 10000
+n_perms = 20000
 
 # load or make the recall matrix
 recalls = af.make_psiturk_recall_matrix(remake_data_file=False, dict_path=dict_path,
                                         save_file='HealEtal16implicit.recalls')
 
 # load or compute the recall dynamics
-all_crps = af.load_the_data(n_perms=n_perms, remake_data_file=False,
+all_crps = af.load_the_data(n_perms=n_perms, remake_data_file=True,
                             recalls_file='HealEtal16implicit.recalls.pkl', save_name=results_dir)
 
 # convert to xarray to make compatible with cbcc_tools --- then run RDF analyses
@@ -40,70 +40,70 @@ all_crps = af.sample_size_table(all_crps, results_dir, recalls)
 
 
 
-
-from scipy.stats import ttest_ind
-
-
-# list 1 vs list 2 for explicit
-c1 = all_crps[np.logical_and(
-    np.logical_and(
-        np.logical_and(all_crps.task_condition=="Shoebox",
-                       all_crps.instruction_condition == "Explicit"),
-        all_crps.list==0),
-    all_crps.lag==0)].all_tf_z.values
-c2 = all_crps[np.logical_and(
-    np.logical_and(
-        np.logical_and(all_crps.task_condition=="Shoebox",
-                       all_crps.instruction_condition == "Explicit"),
-        all_crps.list==1),
-    all_crps.lag==0)].all_tf_z.values
-ttest_ind(c1[~np.isnan(c1)], c2[~np.isnan(c2)])
-
-# list 1 vs list 2 for explicit
-c1 = all_crps[np.logical_and(
-    np.logical_and(
-        np.logical_and(all_crps.task_condition=="Shoebox",
-                       all_crps.instruction_condition == "Incidental"),
-        all_crps.list==0),
-    all_crps.lag==0)].all_tf_z.values
-c2 = all_crps[np.logical_and(
-    np.logical_and(
-        np.logical_and(all_crps.task_condition=="Shoebox",
-                       all_crps.instruction_condition == "Incidental"),
-        all_crps.list==1),
-    all_crps.lag==0)].all_tf_z.values
-ttest_ind(c1[~np.isnan(c1)], c2[~np.isnan(c2)])
-
-
-these_conds = ["Movie", "Scenario", "Animacy", "Weight"]
-ds = []
-for cond in these_conds:
-    c = all_crps[np.logical_and(
-        np.logical_and(
-            np.logical_and(all_crps.task_condition == cond,
-                           all_crps.instruction_condition == "Incidental"),
-            all_crps.list == 0),
-        all_crps.lag == 0)].all_tf_z
-    ds.append(c.mean() / c.std())
-mean_ds = np.mean(ds)
-std_ds = np.std(ds, ddof=1)
-CI_ds = std_ds / np.sqrt(len(ds)) * 1.96
-pm = [mean_ds-CI_ds, mean_ds+CI_ds]
-
-
-
-these_conds = ["Shoebox", "Front Door"]
-ds = []
-for cond in these_conds:
-    c = all_crps[np.logical_and(
-        np.logical_and(
-            np.logical_and(all_crps.task_condition == cond,
-                           all_crps.instruction_condition == "Incidental"),
-            all_crps.list == 0),
-        all_crps.lag == 0)].all_tf_z
-    ds.append(c.mean() / c.std())
-mean_ds = np.mean(ds)
-std_ds = np.std(ds)
+##### stats stuff
+# from scipy.stats import ttest_ind
+#
+#
+# # list 1 vs list 2 for explicit
+# c1 = all_crps[np.logical_and(
+#     np.logical_and(
+#         np.logical_and(all_crps.task_condition=="Shoebox",
+#                        all_crps.instruction_condition == "Explicit"),
+#         all_crps.list==0),
+#     all_crps.lag==0)].all_tf_z.values
+# c2 = all_crps[np.logical_and(
+#     np.logical_and(
+#         np.logical_and(all_crps.task_condition=="Shoebox",
+#                        all_crps.instruction_condition == "Explicit"),
+#         all_crps.list==1),
+#     all_crps.lag==0)].all_tf_z.values
+# ttest_ind(c1[~np.isnan(c1)], c2[~np.isnan(c2)])
+#
+# # list 1 vs list 2 for explicit
+# c1 = all_crps[np.logical_and(
+#     np.logical_and(
+#         np.logical_and(all_crps.task_condition=="Shoebox",
+#                        all_crps.instruction_condition == "Incidental"),
+#         all_crps.list==0),
+#     all_crps.lag==0)].all_tf_z.values
+# c2 = all_crps[np.logical_and(
+#     np.logical_and(
+#         np.logical_and(all_crps.task_condition=="Shoebox",
+#                        all_crps.instruction_condition == "Incidental"),
+#         all_crps.list==1),
+#     all_crps.lag==0)].all_tf_z.values
+# ttest_ind(c1[~np.isnan(c1)], c2[~np.isnan(c2)])
+#
+#
+# these_conds = ["Movie", "Scenario", "Animacy", "Weight"]
+# ds = []
+# for cond in these_conds:
+#     c = all_crps[np.logical_and(
+#         np.logical_and(
+#             np.logical_and(all_crps.task_condition == cond,
+#                            all_crps.instruction_condition == "Incidental"),
+#             all_crps.list == 0),
+#         all_crps.lag == 0)].all_tf_z
+#     ds.append(c.mean() / c.std())
+# mean_ds = np.mean(ds)
+# std_ds = np.std(ds, ddof=1)
+# CI_ds = std_ds / np.sqrt(len(ds)) * 1.96
+# pm = [mean_ds-CI_ds, mean_ds+CI_ds]
+#
+#
+#
+# these_conds = ["Shoebox", "Front Door"]
+# ds = []
+# for cond in these_conds:
+#     c = all_crps[np.logical_and(
+#         np.logical_and(
+#             np.logical_and(all_crps.task_condition == cond,
+#                            all_crps.instruction_condition == "Incidental"),
+#             all_crps.list == 0),
+#         all_crps.lag == 0)].all_tf_z
+#     ds.append(c.mean() / c.std())
+# mean_ds = np.mean(ds)
+# std_ds = np.std(ds)
 
 
 
